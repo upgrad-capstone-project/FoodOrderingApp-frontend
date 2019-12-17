08e190c0c5807fcce855cef1f93aa9976fa75fff
Siva <sivansbhfe@gmail.com>
2019-12-17 21:46:52 +0530

    Commit void

---
+++ src/screens/Controller.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Home from './home/Home';
+import Profile from './profile/Profile';
+import Details from './profile/Details';
+import Checkout from './profile/Checkout';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+class Controller extends Component {
+
+  constructor() {
+    super();
+    this.baseUrl = "testing";
+  }
+  render() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+          <Route exact path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
+		  <Route exact path='/detail' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
+		  <Route exact path='/checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default Controller;
