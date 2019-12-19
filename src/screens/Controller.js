2e2cd87e7057d44e039f2f3c29cf08d3af5979fa
HemaprabhaAshwin <hemaprabha.ashwin@gmail.com>
2019-12-19 12:29:08 +0400

    Incorporated Home and Login page logic

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react';
-import Home from './home/Home';
-import Profile from './profile/Profile';
-import Details from './details/Details';
-import Checkout from './checkout/Checkout';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "../screens/home/Home";
+import Checkout from "../screens/checkout/Checkout";
+import Details from "../screens/details/Details";
+import Profile from "../screens/profile/Profile";
 
 
 class Controller extends Component {
 
-  constructor() {
-    super();
-    this.baseUrl = "testing";
-  }
-  render() {
-    return (
-      <Router>
-        <div className="main-container">
-          <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
-          <Route exact path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
-		  <Route exact path='/detail' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
-		  <Route exact path='/checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
-        </div>
-      </Router>
-    )
-  }
+    constructor(){
+        super();
+        //will have to be replaced with the provided url
+        this.baseUrl= "http://localhost:8080/api/";
+    }
+    
+
+    render(){
+        return(
+            <Router>
+                <div className= "main-container">
+                    <Route exact path="/" render={props=> <Home {...props} baseUrl={this.baseUrl}/>} />
+                    <Route exact path="/restaurant/:id" render={props=> <Details {...props} baseUrl={this.baseUrl}/>} />
+                    <Route exact path="/checkout" render={props=> <Checkout {...props} baseUrl={this.baseUrl}/>} />
+                    <Route exact path="/profile" render={props=> <Profile {...props} baseUrl={this.baseUrl}/>}/>                    
+                </div>
+            </Router>
+
+        );
+    }
 }
 
 export default Controller;
