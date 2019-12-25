beb7e9b45246c64bc8a2fd1224f97f83d3741b9f
Siva <sivansbhfe@gmail.com>
2019-12-25 08:01:46 +0530

    Perfected display of Summary items

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -683,7 +683,7 @@ render(){
                    {item.item.item_type === 'VEG' ?  <FiberManualRecord style={{ color: "#008000" }}/> : <FiberManualRecord style={{ color: "#b20505" }}/>}
                </Grid>
                <Grid item xs={6}>
-                   {item.item.item_name}                        
+                   <span style={{color:"grey", fontSize:20, marginLeft:8}}>{item.item.item_name}</span>                        
                </Grid>
                <Grid item xs={1}>
                    {item.quantity}                      
